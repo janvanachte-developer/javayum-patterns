e70ba505fdb32cd5ffa9381f3457a9dcf7bcca63
jan.vanachte <jan.vanachte@javayum.net>
2017-09-09 10:41:46 +0200

    JAXRS Spring with RequestParam and PathVariable

--- javayum-patterns-web-app-springboot/src/main/java/net/javayum/patterns/web/jaxrs/spring/ItemServiceImpl.java
+++ javayum-patterns-web-app-springboot/src/main/java/net/javayum/patterns/web/jaxrs/spring/ItemServiceImpl.java
@@ -3,20 +3,28 @@ package net.javayum.patterns.web.jaxrs.spring;
 import java.util.concurrent.atomic.AtomicLong;
 
 import net.javayum.patterns.domain.ItemDTO;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/item")
 public class ItemServiceImpl {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/item")
-    public ItemDTO item(@RequestParam(value = "name", defaultValue = "World") String name) {
+    // localhost:8080/item?name=World
+    @RequestMapping(method = RequestMethod.GET)
+    public ItemDTO getByNameWithRequestParam(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new ItemDTO(counter.incrementAndGet(),
                 String.format(template, name));
     }
+
+    // localhost:8080/item/World
+    @RequestMapping(path="/{name}", method = RequestMethod.GET)
+    public ItemDTO getByName(@PathVariable String name) {
+        return new ItemDTO(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+
 }
 
